510321eedb251822245ae0ee114ff254f2893281
Glen Maddern <glenmaddern@gmail.com>
2017-10-11 14:57:46 +1100

    using external store and forceupdate

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -3,49 +3,24 @@ import React from 'react'
 import MessageInput from './MessageInput'
 import ChatWindow from './ChatWindow'
 import './ChatApp.css'
+import store from '../store'
 
 class ChatApp extends React.Component {
-  state = {
-    messages: [
-      {
-        avatar_url: 'http://fillmurray.com/98/98',
-        name: 'Bill Murray',
-        lines: [
-          'Line one',
-          'Line two',
-          'Line three',
-        ]
-      },
-      {
-        avatar_url: 'http://fillmurray.com/100/100',
-        name: 'Bill Murray #2',
-        lines: [
-          'Hey',
-          'There',
-          'Bill!',
-        ]
-      },
-    ]
-  }
-
   handleNewMessage = (message) => {
-    this.setState({
-      messages: this.state.messages.concat({
-        avatar_url: 'http://fillmurray.com/102/102',
-        name: 'Me',
-        lines: [
-          message
-        ]
-      })
+    store.messages.push({
+      avatar_url: 'http://fillmurray.com/102/102',
+      name: 'Me',
+      lines: [
+        message
+      ]
     })
+    this.forceUpdate()
   }
 
   render() {
-    const { messages } = this.state
-
     return (
       <div className="ChatApp_Wrapper">
-        <ChatWindow messages={messages}/>
+        <ChatWindow messages={store.messages}/>
         <MessageInput onNewMessage={this.handleNewMessage}/>
       </div>
     )
---
+++ src/store.js
@@ -0,0 +1,24 @@
+const store = {
+  messages: [
+    {
+      avatar_url: 'http://fillmurray.com/98/98',
+      name: 'Bill Murray',
+      lines: [
+        'Line one',
+        'Line two',
+        'Line three',
+      ]
+    },
+    {
+      avatar_url: 'http://fillmurray.com/100/100',
+      name: 'Bill Murray #2',
+      lines: [
+        'Hey',
+        'There',
+        'Bill!',
+      ]
+    },
+  ]
+}
+
+export default store
