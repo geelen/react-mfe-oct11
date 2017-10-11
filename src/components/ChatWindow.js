35da6a2c5eca62db1a3a67619351629297726e18
Glen Maddern <glenmaddern@gmail.com>
2017-10-11 13:51:47 +1100

    chat window styled up

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -7,16 +7,16 @@ const Lines = 'div'
 
 const ChatWindow = () => (
   <div className="ChatWindow">
-    <Entry>
-      <Avatar>
-        <img src="http://fillmurray.com/98/98" alt="Bill Murray"/>
-      </Avatar>
-      <Lines>
+    <div className="ChatWindow_Entry">
+      <img className="ChatWindow_Avatar"
+           src="http://fillmurray.com/98/98"
+           alt="Bill Murray"/>
+      <div className="ChatWindow_Lines">
         <div>Line one</div>
         <div>Line two</div>
         <div>Line three</div>
-      </Lines>
-    </Entry>
+      </div>
+    </div>
   </div>
 )
 
