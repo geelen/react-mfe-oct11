d25390d3aaa08161ac94f0af616ea8c0881eba02
Glen Maddern <glenmaddern@gmail.com>
2017-10-11 11:48:28 +1100

    breaking out a new component file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import AppHeader from './AppHeader'
 
 const obj = { a: 1, b: 2, c: 3 }
 const foo = ({ a, ...other_stuff }) => {
@@ -9,32 +9,27 @@ const foo = ({ a, ...other_stuff }) => {
 }
 foo(obj)
 
-const AppHeader = () => (
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo"/>
-    <h1 className="App-title">Welcome to React</h1>
-  </header>
-)
-
 class App extends Component {
   state = { clicked: false }
 
   handleClick = () => {
-    this.setState({ clicked: true })
+    this.setState({ clicked: !this.state.clicked })
   }
 
   render() {
+    const greeting = this.state.clicked
+      ? 'WELCOME TO REACT'
+      : 'Welcome to React'
+
     return (
       <div className="App">
-        <AppHeader/>
+        <AppHeader greeting={greeting}/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        {
-          !this.state.clicked
-            ? <p onClick={this.handleClick}>Click me!</p>
-            : <p>I've been clicked!</p>
-        }
+        <p className="App-intro">
+          <button type="button" onClick={this.handleClick}>Click me!</button>
+        </p>
       </div>
     )
   }
